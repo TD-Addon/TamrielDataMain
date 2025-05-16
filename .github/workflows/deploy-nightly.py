b5a48aaa6250f617dca704489ddd479677e5eceb
stele <antoni.sauren@gmail.com>
2025-05-16 23:08:05 +0200

    Changed name and mod date of nightly pack

--- .github/workflows/deploy-nightly.py
+++ .github/workflows/deploy-nightly.py
@@ -59,20 +59,20 @@ if __name__ == "__main__":
         # Update modified time for the ESM
         if file.endswith("Tamriel_Data.esm"):
             print("Updating modified time for Tamriel_Data.esm")
-            os.utime(file, (os.stat(file).st_atime, 1325458800))
+            os.utime(file, (os.stat(file).st_atime, 1325433600))
 
     # Compress the "00 Data Files" directory
     print("Compressing...")
     timestamp = datetime.now().strftime("%Y-%m-%d")
     data_dir = "00 Data Files"
-    zip_path = os.path.abspath(f"Tamriel-Data-Nightly-{timestamp}.7z")
+    zip_path = os.path.abspath(f"Tamriel-Data-Addon-{timestamp}.7z")
     subprocess.run(["7z", "a", zip_path, "./*"], cwd=data_dir, check=True)
 
     print(f"Archive Created: {zip_path}")
 
     # Delete old archives
     for asset in release.get_assets():
-        if "Tamriel-Data-Nightly" in asset.name:
+        if "Tamriel-Data-Addon" in asset.name:
             print(f"Deleting old nightly asset from release: {asset.name}")
             asset.delete_asset()
 
