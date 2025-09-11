f8032ce9eaf7d600c4fb30c13d22ce3d57fde655
hemaris-tr <85847041+hemaris-tr@users.noreply.github.com>
2025-09-10 21:38:29 -0400

    remove meshes with GH support from whitelist

--- 00 Data Files/MWSE/mods/TamrielData/gh/main.lua
+++ 00 Data Files/MWSE/mods/TamrielData/gh/main.lua
@@ -2,28 +2,7 @@
 local common = require("tamrielData.common")
 event.register(tes3.event.initialized, function()
     if common.gh_config then
-		--common.gh_config.blacklist["t_glb_fauna_seabiscuit1"] = false
-		--common.gh_config.blacklist["t_glb_fauna_seabiscuit2"] = false
-		--common.gh_config.blacklist["t_glb_fauna_seabiscuit3"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellauger1"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellauger2"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellauger3"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellauger4"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellckfrg1"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellckfrg2"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellckfrg3"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellckfrg4"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellckfrg5"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellckfrg6"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellcockl1"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellcockl2"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellcockl3"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellcockl4"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellcockl5"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellcockl6"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellconch"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellsnail1"] = false
-		--common.gh_config.blacklist["t_glb_fauna_shellsnail2"] = false
+
 		--common.gh_config.blacklist["t_mw_fauna_ventworm_01"] = false
 		--common.gh_config.blacklist["t_mw_fauna_ventworm_02"] = false
 		--common.gh_config.blacklist["t_mw_fauna_ventworm_03"] = false
@@ -32,28 +11,6 @@ event.register(tes3.event.initialized, function()
 		--common.gh_config.blacklist["t_pi_fauna_fishslvspd2"] = false
 		--common.gh_config.blacklist["t_pi_fauna_fishslvspd3"] = false
 
-		common.gh_config.whitelist["t_glb_fauna_seabiscuit1"] = true
-		common.gh_config.whitelist["t_glb_fauna_seabiscuit2"] = true
-		common.gh_config.whitelist["t_glb_fauna_seabiscuit3"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellauger1"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellauger2"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellauger3"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellauger4"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellckfrg1"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellckfrg2"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellckfrg3"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellckfrg4"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellckfrg5"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellckfrg6"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellcockl1"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellcockl2"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellcockl3"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellcockl4"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellcockl5"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellcockl6"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellconch"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellsnail1"] = true
-		common.gh_config.whitelist["t_glb_fauna_shellsnail2"] = true
 		common.gh_config.whitelist["t_mw_fauna_ventworm_01"] = true
 		common.gh_config.whitelist["t_mw_fauna_ventworm_02"] = true
 		common.gh_config.whitelist["t_mw_fauna_ventworm_03"] = true
@@ -62,21 +19,5 @@ event.register(tes3.event.initialized, function()
 		common.gh_config.whitelist["t_pi_fauna_fishslvspd2"] = true
 		common.gh_config.whitelist["t_pi_fauna_fishslvspd3"] = true
 
-		common.gh_config.blacklist["t_cyr_fauna_nesttant_01"] = true
-		common.gh_config.blacklist["t_cyr_fauna_nesttant_02"] = true
-		common.gh_config.blacklist["t_cyr_fauna_nesttant_03"] = true
-		common.gh_config.blacklist["t_cyr_fauna_nesttant_04"] = true
-		common.gh_config.blacklist["t_hrbre_set_beehive_01"] = true
-		common.gh_config.blacklist["t_hrbre_set_beehive_02"] = true
-		common.gh_config.blacklist["t_hrbre_set_beehive_03"] = true
-		common.gh_config.blacklist["t_hrbre_set_beehive_04"] = true
-		common.gh_config.blacklist["t_mw_fauna_beehvvm_01"] = true
-		common.gh_config.blacklist["t_mw_fauna_beehvvm_02"] = true
-		common.gh_config.blacklist["t_mw_fauna_beehvvm_03"] = true
-		common.gh_config.blacklist["t_sky_fauna_beehveart01"] = true
-		common.gh_config.blacklist["t_sky_fauna_beehveart02"] = true
-		common.gh_config.blacklist["t_sky_fauna_beehveart03"] = true
-		common.gh_config.blacklist["t_skynor_set_beehive01"] = true
-		common.gh_config.blacklist["t_skynor_set_beehive02"] = true
     end
 end)
